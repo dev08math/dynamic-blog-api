fb22178ae4d66330829a5e608fda773bf96bc08a
GitDev08 <mathewprince.dev08@gmail.com>
2022-06-18 20:07:21 +0530

    Added docker support for Django and PostgreSQL

---
+++ docker/local/postgres/maintenance/_sourced/countdown.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+ 
+countdown() {
+   local seconds="${1}"
+   local d=$(($(date +%s) + "${seconds}"))
+   while [ "$d" -ge `date +%s` ]; do
+       echo -ne "$(date -u --date @$(($d - `date +%s`)) +%H:%M:%S)\r";
+       sleep 0.1
+   done
+}
