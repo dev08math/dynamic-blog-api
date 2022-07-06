aeb08589860af45022eafa74a351fbde025a126b
Mathew Prince <mathewprince.dev08@gmail.com>
2022-07-06 06:20:42 -0400

    Added search app

--- dynamic-blog-api/core_apps/search/views.py
+++ dynamic-blog-api/core_apps/search/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from drf_haystack import viewsets
+from drf_haystack.filters import HaystackAutocompleteFilter
+from rest_framework import permissions
 
-# Create your views here.
+from core_apps.articles.models import Article
+
+from .serializers import ArticleSearchSerializer
+
+
+class SearchArticleView(viewsets.HaystackViewSet):
+    permission_classes = [permissions.AllowAny]
+    index_models = [Article]
+    serializer_class = ArticleSearchSerializer
+    filter_backends = [HaystackAutocompleteFilter]
